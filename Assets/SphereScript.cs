0de455ac65f6d72ac8d0ae9744a08fa8bc4da729
Zhaoqi Zhu <zhaoqizh@usc.edu>
2020-02-12 21:17:59 -0800

    demo scene

---
+++ Assets/SphereScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = new Vector3(0, 0, 4);
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
