ff5ddd996f49d333f44eaba7854ed3e2a54a575e
Zhaoqi Zhu <zhaoqizh@usc.edu>
2020-02-13 13:56:34 -0800

    Demo Ready

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Vector3 vel = Vector3.Normalize(collision.rigidbody.velocity) * 15;
+        collision.rigidbody.velocity = vel;
+    }
+}
--- Assets/SphereScript.cs
+++ Assets/SphereScript.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class SphereScript : MonoBehaviour
 {
-
+    
     private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = new Vector3(0, 0, 4);
+        rb.velocity = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
@@ -18,8 +25,20 @@ public class SphereScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            transform.position = new Vector3(0, 0, 4);
-            rb.velocity = new Vector3(0, 0, 0);
+            ResetPosition();
         }
+    }
+
+    public void Lauch()
+    {
+        rb.velocity = new Vector3(Camera.main.transform.forward.x * 1,
+            Camera.main.transform.forward.y * 15,
+            Camera.main.transform.forward.z * 15);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Vector3 vel = Vector3.Normalize(rb.velocity) * 15;
+        rb.velocity = vel;
     }
 }
