6574f571e13d65d7e156b3cd12f1d04145dd7d30
Michael <rmswan@usc.edu>
2020-02-13 21:26:03 -0800

    Added prefab and model for boss and simplified behavior a bit.

--- Assets/BossMove.cs
+++ Assets/BossMove.cs
@@ -23,27 +23,27 @@ public class BossMove : MonoBehaviour
         }
         isMoving = true;
         forward = !forward;
-        Vector3 direction = Vector3.back;
+        Vector3 direction;
         if (forward)
         {
-            sideAmt = Random.Range(2, 3);
-            fwdAmt = Random.Range(0, 3);
-            direction = Vector3.left * sideAmt + Vector3.back * fwdAmt;
+            sideAmt = 3; // Random.Range(2, 3);
+            fwdAmt = 3; //Random.Range(0, 3);
+            direction = Vector3.left * sideAmt;// + Vector3.back * fwdAmt;
         }
         else
         {
-            direction = Vector3.right * sideAmt + Vector3.forward * fwdAmt;
+            direction = Vector3.right * sideAmt;// + Vector3.forward * fwdAmt;
         }
 
         Vector3 targetPosition = transform.localPosition + direction;
         Vector3 origPosition = transform.localPosition;
         float timePassed = 0;
-        float totalTime = 0.5f;
+        float totalTime = 2f;
         while (timePassed < totalTime)
         {
             float percentDone = timePassed / totalTime;
-            // transform.localPosition = Vector3.Lerp(origPosition, targetPosition, animCurve.Evaluate(percentDone));
-            transform.localPosition = Vector3.Slerp(origPosition, targetPosition, animCurve.Evaluate(percentDone));
+            transform.localPosition = Vector3.Lerp(origPosition, targetPosition, animCurve.Evaluate(percentDone));
+            //transform.localPosition = Vector3.Slerp(origPosition, targetPosition, animCurve.Evaluate(percentDone));
             yield return new WaitForSeconds(Time.deltaTime);
             timePassed += Time.deltaTime;
         }
