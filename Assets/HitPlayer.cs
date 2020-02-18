39dad336ce00994ff2a3ed12b13cd0fb420435cf
Zhaoqi Zhu <zhaoqizh@usc.edu>
2020-02-17 22:09:17 -0800

    boss pointer & hit box cue

---
+++ Assets/HitPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitPlayer : MonoBehaviour
+{
+    public int flashLength;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            count = 0;
+            GetComponent<Image>().enabled = true;
+        }
+        if (count > flashLength)
+        {
+            GetComponent<Image>().enabled = false;
+        }
+        else
+        {
+            count += 1;
+        }
+        
+    }
+
+}
---
+++ Assets/PointAtBoss.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtBoss : MonoBehaviour
+{
+    private GameObject boss;
+    private GameObject cam;
+
+    private float yAngle = 0;
+
+    public float offsetX = 0;
+    public float offsetY = 0;
+    public float offsetZ = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss = GameObject.Find("Cola Can");
+        cam = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cam.transform.position
+            + cam.transform.rotation * Vector3.forward * offsetZ
+            + cam.transform.rotation * Vector3.up * offsetY
+            + cam.transform.rotation* Vector3.right * offsetX;
+
+        Vector3 relativePos2Boss = boss.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos2Boss, Vector3.up);
+        rotation *= Quaternion.Euler(90, 0, 0) * Quaternion.Euler(0, yAngle += 5, 0);
+        transform.rotation = rotation;
+
+        //
+    }
+}
