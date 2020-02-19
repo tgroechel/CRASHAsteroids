4283cf041b505c9f39ce1df76f5041244e03ef6d
Michael <rmswan@usc.edu>
2020-02-19 01:01:09 -0800

    BehaviorDesigner import and script folder addition.

---
+++ Assets/Behavior Designer Tutorials/Tasks/Seek.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks.Tutorials
+{
+    [TaskCategory("Tutorial")]
+    [TaskIcon("Assets/Behavior Designer Tutorials/Tasks/Editor/{SkinColor}SeekIcon.png")]
+    public class Seek : Action
+    {
+        [Tooltip("The speed of the agent")]
+        public SharedFloat speed = 10;
+        [Tooltip("The angular speed of the agent")]
+        public SharedFloat angularSpeed = 120;
+        [Tooltip("The agent has arrived when the destination is less than the specified amount. This distance should be greater than or equal to the NavMeshAgent StoppingDistance.")]
+        public SharedFloat arriveDistance = 0.2f;
+        [Tooltip("The GameObject that the agent is seeking")]
+        public SharedGameObject target;
+        [Tooltip("If target is null then use the target position")]
+        public SharedVector3 targetPosition;
+
+        // Component references
+        protected UnityEngine.AI.NavMeshAgent navMeshAgent;
+
+        /// <summary>
+        /// Cache the component references.
+        /// </summary>
+        public override void OnAwake()
+        {
+            navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        }
+
+        /// <summary>
+        /// Allow pathfinding to resume.
+        /// </summary>
+        public override void OnStart()
+        {
+            navMeshAgent.speed = speed.Value;
+            navMeshAgent.angularSpeed = angularSpeed.Value;
+#if UNITY_5_1 || UNITY_5_2 || UNITY_5_3 || UNITY_5_4 || UNITY_5_5
+            navMeshAgent.Resume();
+#else
+            navMeshAgent.isStopped = false;
+#endif
+
+            SetDestination(Target());
+        }
+
+        // Seek the destination. Return success once the agent has reached the destination.
+        // Return running if the agent hasn't reached the destination yet
+        public override TaskStatus OnUpdate()
+        {
+            if (HasArrived()) {
+                return TaskStatus.Success;
+            }
+
+            SetDestination(Target());
+
+            return TaskStatus.Running;
+        }
+
+        // Return targetPosition if target is null
+        private Vector3 Target()
+        {
+            if (target.Value != null) {
+                return target.Value.transform.position;
+            }
+            return targetPosition.Value;
+        }
+
+        /// <summary>
+        /// Set a new pathfinding destination.
+        /// </summary>
+        /// <param name="destination">The destination to set.</param>
+        /// <returns>True if the destination is valid.</returns>
+        private bool SetDestination(Vector3 destination)
+        {
+#if UNITY_5_1 || UNITY_5_2 || UNITY_5_3 || UNITY_5_4 || UNITY_5_5
+            navMeshAgent.Resume();
+#else
+            navMeshAgent.isStopped = false;
+#endif
+            return navMeshAgent.SetDestination(destination);
+        }
+
+        /// <summary>
+        /// Has the agent arrived at the destination?
+        /// </summary>
+        /// <returns>True if the agent has arrived at the destination.</returns>
+        private bool HasArrived()
+        {
+            // The path hasn't been computed yet if the path is pending.
+            float remainingDistance;
+            if (navMeshAgent.pathPending) {
+                remainingDistance = float.PositiveInfinity;
+            } else {
+                remainingDistance = navMeshAgent.remainingDistance;
+            }
+
+            return remainingDistance <= arriveDistance.Value;
+        }
+
+        /// <summary>
+        /// Stop pathfinding.
+        /// </summary>
+        private void Stop()
+        {
+            if (navMeshAgent.hasPath) {
+#if UNITY_5_1 || UNITY_5_2 || UNITY_5_3 || UNITY_5_4 || UNITY_5_5
+                navMeshAgent.Stop();
+#else
+                navMeshAgent.isStopped = true;
+#endif
+            }
+        }
+
+        /// <summary>
+        /// The task has ended. Stop moving.
+        /// </summary>
+        public override void OnEnd()
+        {
+            Stop();
+        }
+
+        /// <summary>
+        /// The behavior tree has ended. Stop moving.
+        /// </summary>
+        public override void OnBehaviorComplete()
+        {
+            Stop();
+        }
+    }
+}
