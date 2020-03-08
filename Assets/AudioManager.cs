9a592c119b22b6f355238f005411bdc2e48b9596
Zequn Yu <zequnyu@usc.edu>
2020-03-08 12:50:09 -0700

    Add Audio Manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+// To use sound outside this class:
+// FindObjectOfType<AudioManager>().Play("GameWin");
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Background");
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+        }
+        s.source.Play();
+    }
+}
