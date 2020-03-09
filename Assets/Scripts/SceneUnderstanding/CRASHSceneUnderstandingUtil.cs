1c8729d9110ec3ac582c7a57f77520ead4efa40a
xxx663xxx <xxx663xxx@kimo.com>
2020-03-09 14:18:30 -0700

    Added More Function to Determine Position on Floor and Updated Usage Exmaple

--- Assets/Scripts/SceneUnderstanding/CRASHSceneUnderstandingUtil.cs
+++ Assets/Scripts/SceneUnderstanding/CRASHSceneUnderstandingUtil.cs
@@ -1,29 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CRASHSceneUnderstandingUtil : MonoBehaviour
 {
+    [Tooltip("Prints debug message of this util on console or not.")]
+    public static bool ShowDebugMessage = false;
+
     /// <summary>
     /// Gives the position where robot could be instantiated two meters away from the player and stays on ground
     /// </summary>
-    /// <returns>Feasible instantiate position of the robot</returns>
-    public static Vector3 GetRobotInitialPosition()
+    /// <returns>
+    /// Feasible instantiate position of the robot. 
+    /// If no floor is deteced, returned position would be 2 meters directly in front of player's camera
+    /// </returns>
+    public static Vector3 GetRobotInitialPosition(GameObject robot)
     {
         Vector3 startPoint = Camera.main.transform.position + (Camera.main.transform.forward * 2);
         Vector3 direction = Camera.main.transform.up * -1;
 
+        Tuple<bool, Vector3> result = GetFloorProjectionPoint(startPoint, direction);
+        if (result.Item1)
+        {
+            if(ShowDebugMessage) Debug.Log("Floor detected, use hit point of downard raycast 2 meters away from player as robot initial position");
+            return result.Item2;
+        }
+        else
+        {
+            if (ShowDebugMessage) Debug.Log("Floor not detected, failover to use point 2 meters away from player as robot initial position");
+            return Camera.main.transform.position + (Camera.main.transform.forward * 2);
+        }
+    }
+
+    /// <summary>
+    /// Returns whether a floor plane is detected below the given object and it's projection point on the floor plane.
+    /// </summary>
+    /// <returns>
+    /// A tuple. First item is a bool that indicates whether a floor plane is found.
+    /// Second item is a Vector3 that returns the projection point on floor. If no floor is found, this item would be (0,0,0)
+    /// </returns>
+    public static Tuple<bool, Vector3> GetFloorProjectionPoint(GameObject go)
+    {
+        Vector3 startPoint = go.transform.position;
+        Vector3 direction = go.transform.up * -1;
+
+        return GetFloorProjectionPoint(startPoint, direction);
+    }
+
+    /// <summary>
+    /// Returns whether a floor plane is detected by the raycast starting from "startPoint" to "direction" and raycast hit point
+    /// </summary>
+    /// <returns>
+    /// A tuple. First item is a bool that indicates whether a floor plane is detected.
+    /// Second item is a Vector3 that returns the hit point on floor. If no floor is found, this item would be (0,0,0)
+    /// </returns>
+    public static Tuple<bool, Vector3> GetFloorProjectionPoint(Vector3 startPoint, Vector3 direction)
+    {
         RaycastHit hit;
         if (Physics.Raycast(startPoint, direction, out hit, Mathf.Infinity))
         {
-            Debug.Log("Floor detected, use hit point of downard raycast 2 meters away from player as robot initial position");
             if (hit.collider.gameObject.name == "Floor")
             {
-                return hit.point;
+                if(ShowDebugMessage) Debug.Log("Floor detected");
+                return Tuple.Create(true, hit.point);
             }
         }
 
-       Debug.Log("Floor not detected, failover to use point 2 meters away from player as robot initial position");
-       return Camera.main.transform.position + (Camera.main.transform.forward * 2);
+        if (ShowDebugMessage) Debug.Log("Floor not detected, return object's original position");
+        return Tuple.Create(false, Vector3.zero);
     }
 }
--- Assets/Scripts/SceneUnderstanding/InstanciateAtGround.cs
+++ Assets/Scripts/SceneUnderstanding/InstanciateAtGround.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InstanciateAtGround : MonoBehaviour
 {
+    public float objectHeight = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = CRASHSceneUnderstandingUtil.GetRobotInitialPosition();
+        // Instanciate robot 2 meters away in front of player
+        transform.position = CRASHSceneUnderstandingUtil.GetRobotInitialPosition(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Do anything with this game object's transfrom according to game logic here....
+
+        // Update transform of this game object to make it stay on ground if any floor plane is detected
+        Tuple<bool, Vector3> result = CRASHSceneUnderstandingUtil.GetFloorProjectionPoint(gameObject);
+        
+        // If any floor plane is deteced below this game object
+        if (result.Item1) 
+        {
+            // Get projection point on floor plane and add height of this game object to obtain
+            // the position that makes this game object appear as staying on ground
+            Vector3 floorPos = result.Item2;
+            floorPos.y = floorPos.y + objectHeight;
+            
+            // Move this game object to calculated position
+            transform.position = floorPos;
+        }
         
     }
 }
