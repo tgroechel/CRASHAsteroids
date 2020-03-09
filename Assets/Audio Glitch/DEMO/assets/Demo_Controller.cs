3d41bc6f7f94e7e23b17ddac55c12afdfb101f5a
Zhaoqi Zhu <zhaoqizh@usc.edu>
2020-03-08 20:40:07 -0700

    SHOOTING BULLETS IN 3 PATTERNS

---
+++ Assets/Audio Glitch/DEMO/assets/Demo_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Demo_Controller : MonoBehaviour
+{
+
+	public AudioGlitch audio_glitch;
+	public Button btn_start;
+	public Button btn_end;
+
+
+	void Start ()
+	{
+		btn_start.onClick.AddListener (()=>{	audio_glitch.Start_Process();	});
+		btn_end.onClick.AddListener   (()=>{	audio_glitch.Stop_Process();    });
+	}
+
+}
