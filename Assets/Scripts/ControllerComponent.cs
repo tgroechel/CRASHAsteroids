4f630e618a09f78ea1815849144f5be0f28870f6
Michael <rmswan@usc.edu>
2020-03-11 19:39:42 -0700

    Snap work

---
+++ Assets/Scripts/ControllerComponent.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Whatever {
+    public class ControllerComponent : MonoBehaviour {
+
+        public static ControllerComponent curDraggingControllerComponent;
+        ManipulationHandler manipulationHandler;
+        List<SnapCollider> mySnapColliders;
+        SnapCollider snapColInContact;
+        HashSet<ControllerComponent> attachedComponents;
+
+        private void Start() {
+            manipulationHandler = GetComponent<ManipulationHandler>();
+            manipulationHandler.OnManipulationStarted.AddListener(OnManipulationStart);
+            manipulationHandler.OnManipulationEnded.AddListener(OnManipulationEnd);
+        }
+
+        public List<SnapCollider> GetMySnapColliders() {
+            if (mySnapColliders == null) {
+                mySnapColliders = GetComponentsInChildren<SnapCollider>().ToList();
+            }
+            return mySnapColliders;
+        }
+
+        public HashSet<ControllerComponent> GetAttachedControllerComponents() {
+            if (attachedComponents == null) {
+                attachedComponents = new HashSet<ControllerComponent>();
+            }
+            return attachedComponents;
+        }
+
+        public void AddComponentToAttached(ControllerComponent cc) {
+            GetAttachedControllerComponents().Add(cc);
+        }
+
+        public void RemoveComponentAttached(ControllerComponent cc) {
+            GetAttachedControllerComponents().Remove(cc);
+        }
+
+        private void OnManipulationStart(ManipulationEventData arg0) {
+            curDraggingControllerComponent = this;
+            SnapColliderManager.instance.EnableColliders();
+            DisableMySnapColliders();
+        }
+
+        private void OnManipulationEnd(ManipulationEventData arg0) {
+            curDraggingControllerComponent = null;
+            if (snapColInContact == null) {
+                RemoveFromParentControllerComponent();
+                transform.SnapToSnapManager();
+            }
+            else {
+                snapColInContact.DoSnapAction();
+                snapColInContact = null;
+            }
+            SnapColliderManager.instance.DisableColliders();
+        }
+
+
+        public void RemoveFromParentControllerComponent() {
+            ControllerComponent cc = GetParentControllerComponent();
+            if (cc) {
+                cc.RemoveComponentAttached(this);
+            }
+        }
+
+        public ControllerComponent GetParentControllerComponent() {
+            return transform.parent?.parent?.GetComponent<ControllerComponent>();
+        }
+
+        public void SetSnapColInContact(SnapCollider sc) {
+            snapColInContact = sc;
+        }
+
+        void DisableMySnapColliders() {
+            ToggleMySnapColliders(false);
+        }
+
+        void EnableMySnapColliders() {
+            ToggleMySnapColliders(true);
+        }
+
+        void ToggleMySnapColliders(bool b) {
+            foreach (SnapCollider sc in GetMySnapColliders()) {
+                sc.gameObject.SetActive(b);
+            }
+            foreach (ControllerComponent cc in GetAttachedControllerComponents()) {
+                cc.ToggleMySnapColliders(b);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CustomTwistPublisher.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace RosSharp.RosBridgeClient {
+    public class CustomTwistPublisher : Publisher<Messages.Geometry.Twist> {
+
+        private Messages.Geometry.Twist message;
+        public Vector3 linearTwist;
+        public Vector3 angularTwist;
+
+        protected override void Start() {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Alpha0)) {
+                UpdateMessage();
+            }
+        }
+
+        private void InitializeMessage() {
+            message = new Messages.Geometry.Twist();
+            message.linear = new Messages.Geometry.Vector3();
+            message.angular = new Messages.Geometry.Vector3();
+        }
+
+        private void UpdateMessage() {
+            message.linear = GetGeometryVector3(linearTwist.Unity2Ros()); ;
+            message.angular = GetGeometryVector3(angularTwist.Unity2Ros());
+            Publish(message);
+            Debug.Log(message);
+        }
+        public void Forward() {
+            PublishTwist(Vector3.forward, Vector3.zero);
+        }
+        public void Back() {
+            PublishTwist(Vector3.back, Vector3.zero);
+        }
+
+        public void Right() {
+            PublishTwist(Vector3.zero, new Vector3(0, -Mathf.PI / 2, 0));
+        }
+
+        public void Left() {
+            PublishTwist(Vector3.zero, new Vector3(0, Mathf.PI / 2, 0));
+        }
+
+        public void PublishTwist(Vector3 lin, Vector3 ang) {
+            linearTwist = lin;
+            angularTwist = ang;
+            UpdateMessage();
+        }
+
+        private static Messages.Geometry.Vector3 GetGeometryVector3(Vector3 vector3) {
+            Messages.Geometry.Vector3 geometryVector3 = new Messages.Geometry.Vector3();
+            geometryVector3.x = (float)vector3.x;
+            geometryVector3.y = (float)vector3.y;
+            geometryVector3.z = (float)vector3.z;
+            return geometryVector3;
+        }
+    }
+}
