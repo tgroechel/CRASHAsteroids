94befbb982287a394c1881906cd1db5be04c3efd
Thomas Groechel <trgroechel@gmail.com>
2020-03-29 12:51:40 -0700

    Updated for basic controller components for velocity

--- Assets/KuriManager.cs
+++ Assets/KuriManager.cs
@@ -7,13 +7,15 @@ using UnityEngine.Assertions;
 namespace Crash {
     public class KuriManager : Singleton<KuriManager> {
         Rigidbody rb;
-        public static float MAX_SPEED = 1.0f, MASS = 3.0f, ANGLE_EPSILON = 2.0f;
-        public float desiredVelZ;
-        public float desiredRotation;
+        public static float MAX_SPEED = 1.0f, MASS = 3.0f, ANGLE_EPSILON = 5.0f, KDP = 0.2f;
+        public float KDD = 0.01f, KDI = 0.01f;
+        public float lastError, i_error;
+        public float desiredVelZ, desiredRotation;
         Vector3 forceToApply, rotationToApply;
         void Awake() {
             rb = GetComponent<Rigidbody>();
         }
+
         private void Update() {
             if (Input.GetKeyDown(KeyCode.Alpha0)) {
                 // PublishVelocityCommand(desiredVelZ, desiredRotation);
@@ -25,10 +27,11 @@ namespace Crash {
             desiredRotation = rotationAngle;
         }
 
-        private void FixedUpdate() {
-            rb.inertiaTensor = rb.inertiaTensor;
+        public void SetVelocity(float linear) {
+            desiredVelZ = linear;
+        }
 
-            rb.inertiaTensorRotation = rb.inertiaTensorRotation;
+        private void FixedUpdate() {
             if (Mathf.Abs(rb.velocity.z) <= Mathf.Abs(desiredVelZ)) {
                 forceToApply.z = desiredVelZ * MASS;
                 rb.AddForce(forceToApply, ForceMode.VelocityChange);
@@ -36,11 +39,18 @@ namespace Crash {
 
             if (desiredRotation < transform.rotation.eulerAngles.y - ANGLE_EPSILON ||
                 desiredRotation > transform.rotation.eulerAngles.y + ANGLE_EPSILON) {
-                //rotationToApply.y = 1;
+                float p_error = desiredRotation - transform.rotation.eulerAngles.y;
+                float d_error = p_error - lastError;
+                i_error += (p_error * Time.fixedDeltaTime);
+                lastError = p_error;
+                Debug.Log(d_error);
+                rotationToApply.y = KDP * p_error + KDD * d_error + KDI * i_error;
                 rb.AddTorque(rotationToApply, ForceMode.VelocityChange);
             }
             else {
                 rotationToApply.y = 0;
+                lastError = 0;
+                i_error = 0;
                 rb.AddTorque(rotationToApply, ForceMode.VelocityChange);
             }
         }
