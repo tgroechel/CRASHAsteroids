15078e9ff7f6f55fd7b5309509015c7a111c5442
Siddhant Shenoy <siddhant8897@gmail.com>
2020-03-31 16:16:18 -0700

    Spider now walks on walls and floor using NavMesh and links!

---
+++ Assets/Scripts/BakeNavMeshRuntime.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using eDmitriyAssets.NavmeshLinksGenerator;
+
+public class BakeNavMeshRuntime : MonoBehaviour
+{
+    private NavMeshSurface surface;
+    private NavMeshLinks_AutoPlacer autoplacer;
+    private NavMeshLinks_AutoPlacer_Editor editor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the NavMeshSurface component
+        surface = GetComponent<NavMeshSurface>();
+
+        // Get the NavMeshLinks_AutoPlacer component
+        autoplacer = GetComponent<NavMeshLinks_AutoPlacer>();
+
+        // Get the NavMeshLinks_AutoPlacer_Editor component
+        editor = GetComponent<NavMeshLinks_AutoPlacer_Editor>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            // Build NavMesh
+            surface.BuildNavMesh();
+        }
+        else if(Input.GetKeyDown(KeyCode.G))
+        {
+            // Generate NavMesh Links
+            GetComponent<NavMeshLinks_AutoPlacer>().Generate();
+        }
+        else if(Input.GetKeyDown(KeyCode.N))
+        {
+            // Remove NavMesh
+            surface.RemoveData();
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            // Remove NavMesh Links
+            GetComponent<NavMeshLinks_AutoPlacer>().ClearLinks();
+        }
+    }
+}
---
+++ Assets/Scripts/MinionFollowPlayerScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class MinionFollowPlayerScript : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    public Animator animator;
+    public bool followPlayer;
+    public bool isonfloor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.updateRotation = false;
+        followPlayer = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !followPlayer)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if(followPlayer)
+        {
+            // Agent follows the player
+            agent.SetDestination(Camera.main.transform.position);
+        }
+
+        // The object moves due to its animation to positions determined by the navmesh agent
+        if (agent.remainingDistance > agent.stoppingDistance)
+            character.Move(agent.desiredVelocity, false, false);
+        else
+            character.Move(Vector3.zero, false, false);
+
+    }
+}
