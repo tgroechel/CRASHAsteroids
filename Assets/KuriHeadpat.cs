c159948d03b17f6bb4bc4e7681d8bf2a5455f9cb
Thomas Groechel <trgroechel@gmail.com>
2020-04-01 16:42:52 -0700

    Added head pat

---
+++ Assets/KuriHeadpat.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crash {
+    public class KuriHeadpat : MonoBehaviour, IMixedRealityTouchHandler {
+        NearInteractionTouchable nearInteractionTouchable;
+
+        public void OnTouchCompleted(HandTrackingInputEventData eventData) {
+
+        }
+
+        public void OnTouchStarted(HandTrackingInputEventData eventData) {
+
+        }
+
+        public void OnTouchUpdated(HandTrackingInputEventData eventData) {
+
+        }
+
+    }
+}
--- Assets/KuriManager.cs
+++ Assets/KuriManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,53 +8,28 @@ using UnityEngine.Assertions;
 namespace Crash {
     public class KuriManager : Singleton<KuriManager> {
         Rigidbody rb;
-        public static float MAX_SPEED = 1.0f, MASS = 3.0f, ANGLE_EPSILON = 5.0f, KDP = 0.2f;
-        public float KDD = 0.01f, KDI = 0.01f;
-        public float lastError, i_error;
-        public float desiredVelZ, desiredRotation;
-        Vector3 forceToApply, rotationToApply;
+        public static float MAX_SPEED = 1.0f, MASS = 100.0f, ANGLE_EPSILON = 5.0f;
+        KuriWheelController kuriWheelController;
+
         void Awake() {
             rb = GetComponent<Rigidbody>();
+            kuriWheelController = GetComponent<KuriWheelController>();
+        }
+
+        internal void AlertHeadPat() {
+            Debug.Log("Head was pat");
         }
 
         private void Update() {
-            if (Input.GetKeyDown(KeyCode.Alpha0)) {
-                // PublishVelocityCommand(desiredVelZ, desiredRotation);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) {
+                SetVelocity(1);
             }
-        }
-
-        public void PublishVelocityCommand(float linear, float rotationAngle) {
-            desiredVelZ = linear;
-            desiredRotation = rotationAngle;
         }
 
         public void SetVelocity(float linear) {
-            desiredVelZ = linear;
+
         }
 
-        private void FixedUpdate() {
-            if (Mathf.Abs(rb.velocity.z) <= Mathf.Abs(desiredVelZ)) {
-                forceToApply.z = desiredVelZ * MASS;
-                rb.AddForce(forceToApply, ForceMode.VelocityChange);
-            }
-
-            if (desiredRotation < transform.rotation.eulerAngles.y - ANGLE_EPSILON ||
-                desiredRotation > transform.rotation.eulerAngles.y + ANGLE_EPSILON) {
-                float p_error = desiredRotation - transform.rotation.eulerAngles.y;
-                float d_error = p_error - lastError;
-                i_error += (p_error * Time.fixedDeltaTime);
-                lastError = p_error;
-                Debug.Log(d_error);
-                rotationToApply.y = KDP * p_error + KDD * d_error + KDI * i_error;
-                rb.AddTorque(rotationToApply, ForceMode.VelocityChange);
-            }
-            else {
-                rotationToApply.y = 0;
-                lastError = 0;
-                i_error = 0;
-                rb.AddTorque(rotationToApply, ForceMode.VelocityChange);
-            }
-        }
 
 
     }
