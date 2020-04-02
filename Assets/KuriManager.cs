55ac511d9f721632bf6962e446f5a5bc8ca60f32
Thomas Groechel <trgroechel@gmail.com>
2020-04-01 17:46:49 -0700

    Turn, speed, and pat done for Kuri

--- Assets/KuriManager.cs
+++ Assets/KuriManager.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.Assertions;
+﻿using UnityEngine;
 
 namespace Crash {
     public class KuriManager : Singleton<KuriManager> {
         Rigidbody rb;
-        public static float MAX_SPEED = 1.0f, MASS = 100.0f, ANGLE_EPSILON = 5.0f;
+        public const float MAX_SPEED = 1.0f, MASS = 100.0f, ANGLE_EPSILON = 2.0f, MAX_TURN = 360, MIN_TURN = 0;
         KuriWheelController kuriWheelController;
+
+        [Range(-MAX_SPEED, MAX_SPEED)]
+        public float desiredSpeed;
+
+        [Range(MIN_TURN, MAX_TURN)]
+        public float desiredRotation;
+
+        public float turnSpeed = 3.0f;
 
         void Awake() {
             rb = GetComponent<Rigidbody>();
@@ -20,17 +23,35 @@ namespace Crash {
             Debug.Log("Head was pat");
         }
 
-        private void Update() {
-            if (Input.GetKeyDown(KeyCode.Alpha2)) {
-                SetVelocity(1);
+        public void SetVelocity(float linear) {
+            if (linear >= 0) {
+                desiredSpeed = Mathf.Min(linear, MAX_SPEED);
+            }
+            else {
+                desiredSpeed = Mathf.Max(linear, -MAX_SPEED);
             }
         }
 
-        public void SetVelocity(float linear) {
-
+        public void SetRotation(float rotation) {
+            desiredRotation = rotation;
         }
 
+        private void FixedUpdate() {
+            UpdateVelocity();
+            Turn();
+        }
 
+        private void UpdateVelocity() {
+            rb.velocity = transform.forward * desiredSpeed;
+        }
 
+        private void Turn() {
+            float error = ((desiredRotation + transform.rotation.eulerAngles.y) % 360) - 180;
+            float turnAngle = error > 0 ? -turnSpeed : turnSpeed;
+            if (Mathf.Abs(error) > ANGLE_EPSILON) {
+                transform.Rotate(Vector3.up, turnAngle, Space.Self);
+            }
+
+        }
     }
 }
