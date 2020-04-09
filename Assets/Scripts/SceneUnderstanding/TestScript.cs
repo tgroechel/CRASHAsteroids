c0b7b0d9ea770857fdc4c3e030725a6f35ec5d3d
xxx663xxx <xxx663xxx@kimo.com>
2020-04-09 03:20:59 -0700

    Added Calibration Scene

---
+++ Assets/Scripts/SceneUnderstanding/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject root = GameObject.Find("SceneUnderstanding");
+        if (root != null)
+        {
+            Debug.Log("SceneUnderstanding Found");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
