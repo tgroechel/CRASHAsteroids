fef33f118ea3a66f4f4ba55c49800ff8ae6bcac2
Thomas Groechel <groechel@usc.edu>
2020-04-15 14:05:58 -0700

    Basic player health stuff

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crash {
+    public class PlayerHealth : MonoBehaviour {
+        public const float MAX_PLAYER_HEALTH = 100;
+        public float curHealth = 100;
+        public float rechargeTimeInterval = 5;
+        private float curTimeUntilRecharge;
+        public bool rechargeCoroutineRunning = false;
+
+        public void GotHit(float damage) {
+            curHealth -= damage;
+            StartCoroutine(RechargeAfterTime());
+        }
+
+        IEnumerator RechargeAfterTime() {
+            if (rechargeCoroutineRunning) {
+                // reset 
+                curTimeUntilRecharge = rechargeTimeInterval;
+                yield break;
+            }
+            curTimeUntilRecharge = rechargeTimeInterval;
+            while (curTimeUntilRecharge > 0) {
+                yield return null;
+                Debug.Log(curHealth);
+                curTimeUntilRecharge -= Time.deltaTime;
+            }
+            curHealth = 100;
+            rechargeCoroutineRunning = false;
+        }
+    }
+}
