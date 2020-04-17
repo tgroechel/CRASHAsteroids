578fb283617b53e41ef275113b5c912dc9743597
Siddhant Shenoy <siddhant8897@gmail.com>
2020-04-16 18:33:30 -0700

    Added new emeies sound script

---
+++ Assets/Scripts/EnemySoundsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sid
+{
+    public class EnemySoundsScript : MonoBehaviour
+    {
+        public AudioClip[] walkClips;
+        public AudioClip[] rollClips;
+        public AudioSource legSource;
+
+        private void playWalkSound()
+        {
+            AudioClip clip = GetRandomClip(walkClips);
+            legSource.PlayOneShot(clip);
+        }
+
+        private void playRollSound()
+        {
+            AudioClip clip = GetRandomClip(rollClips);
+            legSource.PlayOneShot(clip);
+        }
+
+        private AudioClip GetRandomClip(AudioClip[] clips)
+        {
+            return clips[UnityEngine.Random.Range(0, clips.Length)];
+        }
+    }
+}
