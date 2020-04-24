2c6fac3247a10d4f05080fa3d09b4b6dfdb019b1
Michael Swan <rmswan@usc.edu>
2020-04-23 17:25:54 -0700

    Updated Boss behavior tree and helper scripts to deactivate boss/laser properly and handle HP events better.

---
+++ Assets/Scripts/Tasks/SpawnMiniFromBoss.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace Crash
+{
+    public class SpawnMiniFromBoss : Action
+    {
+        public Object lastSpawnedMinion;
+        private MinionSpawner minionSpawner;
+
+        public override void OnAwake()
+        {
+            minionSpawner = GetComponent<MinionSpawner>();
+        }
+
+        public override void OnStart()
+        {
+            lastSpawnedMinion = minionSpawner.SpawnMinion();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            return TaskStatus.Success;
+        }
+    }
+}
