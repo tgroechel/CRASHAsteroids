88e6a707cc6564077cfa50e43bb594974554cb75
Zhaoqi Zhu <zhaoqizh@usc.edu>
2020-04-24 18:40:40 -0700

    better minispider rotation and bug fixes

--- Assets/MiniBossAttacks.cs
+++ Assets/MiniBossAttacks.cs
@@ -58,15 +58,31 @@ namespace Crash
 
             if (rotatingToPlayer)
             {
-                
-                top.transform.localRotation = Quaternion.Euler(0, lookAtPlayerRotation2.eulerAngles.y + 180, 0);// Quaternion.Euler(gameObject.transform.eulerAngles.x, lookAtPlayerRotation2.eulerAngles.y, gameObject.transform.eulerAngles.z);// * gameObject.transform.rotation * Quaternion.Euler(0,180,0);
+
+                //top.transform.localRotation = Quaternion.Euler(0, lookAtPlayerRotation2.eulerAngles.y - transform.rotation.eulerAngles.y, 0);// Quaternion.Euler(gameObject.transform.eulerAngles.x, lookAtPlayerRotation2.eulerAngles.y, gameObject.transform.eulerAngles.z);// * gameObject.transform.rotation * Quaternion.Euler(0,180,0);
+                top.transform.rotation = lookAtPlayerRotation2;
+                float xxx = top.transform.localRotation.eulerAngles.x;
+                if (xxx > 180)
+                    xxx -= 360;
+                if (Mathf.Abs(xxx) > 20)
+                    xxx = 20 * Mathf.Sign(xxx);
+                float zzz = top.transform.localRotation.eulerAngles.z;
+                if (zzz > 180)
+                    zzz -= 360;
+                if (Mathf.Abs(zzz) > 20)
+                    zzz = 20 * Mathf.Sign(zzz);
+
+                top.transform.localRotation = Quaternion.Euler(
+                    xxx,
+                    top.transform.localRotation.eulerAngles.y,
+                    zzz);
 
                 gunGO.transform.rotation = lookAtPlayerRotation;// Quaternion.Euler(lookAtPlayerRotation.eulerAngles.x, top.transform.rotation.eulerAngles.y, lookAtPlayerRotation.eulerAngles.z);
                 float yyy = gunGO.transform.localRotation.eulerAngles.y;
                 if (yyy > 180)
                     yyy -= 360;
-                if (Mathf.Abs(yyy) > 50)
-                    yyy = 50 * Mathf.Sign(yyy);
+                if (Mathf.Abs(yyy) > 40)
+                    yyy = 40 * Mathf.Sign(yyy);
                 
                 gunGO.transform.localRotation = Quaternion.Euler(
                     gunGO.transform.localRotation.eulerAngles.x,
